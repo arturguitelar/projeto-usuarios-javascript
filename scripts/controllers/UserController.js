b3b6a17014e0f53390594b1c35528d7e562bc165
Artur Killdragon <artur.guitelar@gmail.com>
2019-04-16 17:36:38 -0300

    Utilizando a api FileReader para mockar o upload de uma imagem.

--- scripts/controllers/UserController.js
+++ scripts/controllers/UserController.js
@@ -24,28 +24,29 @@ class UserController {
 
         let user = {};
 
-        // Esta é a forma que está no exercício mas não funcionou.
-        // this.formEl.elements.forEach(function(field, index) {
-        //     if (field.name == 'gender') {
-        //         if (field.checked) {
-        //             user[field.name] = field.value;
-        //         }
-        //     } else {
-        //         user[field.name] = field.value;
-        //     }          
-        // });
-
-        for (let i = 0; i < this.formEl.elements.length; i++) {
-            let field = this.formEl.elements[i];
-
+        // Solução com spread operator
+        [...this.formEl.elements].forEach(function(field, index) {
             if (field.name == 'gender') {
                 if (field.checked) {
                     user[field.name] = field.value;
                 }
             } else {
                 user[field.name] = field.value;
-            } 
-        }
+            }          
+        });
+
+        // // Solução com um laço for
+        // for (let i = 0; i < this.formEl.elements.length; i++) {
+        //     let field = this.formEl.elements[i];
+
+        //     if (field.name == 'gender') {
+        //         if (field.checked) {
+        //             user[field.name] = field.value;
+        //         }
+        //     } else {
+        //         user[field.name] = field.value;
+        //     } 
+        // }
 
         return new User(
             user.name, 
@@ -60,6 +61,29 @@ class UserController {
     }
 
     /**
+     * Utiliza a api FileReader para trazer uma imagem
+     * selecionada pelo usuário para o frontend.
+     * 
+     * @param {Function} callback 
+     */
+    getPhoto(callback) {
+        let fileReader = new FileReader();
+
+        // buscando o elemento photo
+        let elements = [...this.formEl.elements].filter(item => {
+            if (item.name === 'photo') return item;
+        });
+
+        let file = elements[0].files[0];
+        
+        fileReader.onload = () => {
+            callback(fileReader.result);
+        };
+
+        fileReader.readAsDataURL(file);
+    }
+
+    /**
      * Cria um elemento "tr" para uma tabela especificada utilizando os dados 
      * que foram preenchidos no formulário de criação de usuário.
      * 
@@ -69,7 +93,7 @@ class UserController {
 
         this.tableEl.innerHTML = `
             <tr>
-                <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
                 <td>${dataUser.admin}</td>
@@ -90,7 +114,14 @@ class UserController {
         this.formEl.addEventListener('submit', event => {
             event.preventDefault();
 
-            this.addLine(this.getValues());
+            let values = this.getValues();
+            
+            this.getPhoto(content => {
+                values.photo = content;
+
+                this.addLine(values);
+            });
+
         });
     }
 }
