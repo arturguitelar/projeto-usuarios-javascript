680edf7f5e9ac383af27110ca7db351769f0065b
Artur Killdragon <artur.guitelar@gmail.com>
2019-04-16 14:36:33 -0300

    Adicionando visualmente um usuário na lista de usuários. Criação de classe User como modelo.

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Projeto baseado no curso de Javascript da HCode.
 Algumas coisas interessantes aprendidas durante o projeto:
 - Percorrer os campos de um formulário dinamicamente e criar um objeto com chaves dinâmicas através do resultado.
 - Abordagem simples ao conceito de SPA (Single Page Aplication).
+- Template strings para criação dinâmica de elementos.
+- Abordagem aos conceitos de OOP e MVC.
 
 ### [Perfil da Hcode no github.](https://github.com/hcodebr)
 
--- scripts/index.js
+++ scripts/index.js
@@ -18,5 +18,41 @@ document.getElementById('form-user-create').addEventListener('submit', function(
         
     });
 
-    console.log(user);
+    var objectUser = new User(
+        user.name, 
+        user.gender, 
+        user.birth, 
+        user.country, 
+        user.email, 
+        user.password, 
+        user.photo, 
+        user.admin
+    );
+
+    addLine(objectUser);
 });
+
+/**
+ * Cria um elemento "tr" na tabela utilizando os dados 
+ * que foram preenchidos no formulário de criação de usuário.
+ * 
+ * @param {User} dataUser Objeto com dados do Usuário.
+ */
+function addLine(dataUser) {
+
+    console.log(dataUser);
+    
+    document.getElementById('table-users').innerHTML = `
+        <tr>
+            <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>
+    `;
+}
