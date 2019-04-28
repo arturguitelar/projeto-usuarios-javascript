b30fb2a7a35467847a18db7ea4d8158503abaaea
Artur Killdragon <artur.guitelar@gmail.com>
2019-04-28 07:54:22 -0300

    Create: Classe HTTPRequest para requisições ajax ao banco de dados.

--- restapi/utils/validator.js
+++ restapi/utils/validator.js
@@ -1,8 +1,8 @@
 module.exports = {
     user: (app, req, res) => {
         // validação de campos utilizando express-validator
-        req.assert('name', 'O nome é obrigatório.').notEmpty();
-        req.assert('email', 'O e-mail está inválido.').notEmpty().isEmail();
+        req.assert('_name', 'O nome é obrigatório.').notEmpty();
+        req.assert('_email', 'O e-mail está inválido.').notEmpty().isEmail();
 
         let errors = req.validationErrors();
         // se existir erros retorna um array, caso contrário retorna true
